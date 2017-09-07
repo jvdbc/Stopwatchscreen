831a1e733f91b92a3a37a71ef9df1b575701fe82
Julien Vdbc <julien-externe.vasdeboncoeur@erdf.fr>
2017-09-07 11:44:52 +0200

    * First commit * Every is hard code * Better font and choice of message should be appreciate

---
+++ Stopwatchscreen/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20Min
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly SolidColorBrush _red = new SolidColorBrush(Colors.DarkRed);
+        private readonly SolidColorBrush _black = new SolidColorBrush(Colors.Black);
+        private System.Drawing.Point _lm;
+        private bool _clicado = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var ttla = new TimeToLookAway();
+            var ctrLeft = this.Left;
+            var ctrTop = this.Top;
+            ttla.LookAway += (o, args) =>
+            {
+                this.Dispatcher.Invoke(
+                    async () =>
+                    {
+                        this.Activate();
+                        this.Topmost = true;
+
+                        this.Left = SystemParameters.PrimaryScreenWidth / 2 - Width / 2;
+                        this.Top =  SystemParameters.PrimaryScreenHeight / 2 - Height / 2;
+
+                        TextBlock.Foreground = _red;
+                        TextBlock.FontSize += TextBlock.FontSize;
+
+                        await Task.Delay(20000).ContinueWith(t =>
+                        {
+                            this.Dispatcher.Invoke(
+                                () =>
+                                {
+                                    this.Topmost = false;
+                                    TextBlock.Foreground = _black;
+                                    TextBlock.FontSize = TextBlock.FontSize/2;
+                                });
+                        });
+                    }
+                    );
+            };
+
+            this.DataContext = ttla;
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(
+            IntPtr hWnd, IntPtr hWndInsertAfter,
+            int X, int Y,
+            int cx, int cy,
+            uint uFlags);
+
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+
+        static void SendWpfWindowBack(Window window)
+        {
+            var hWnd = new WindowInteropHelper(window).Handle;
+            SetWindowPos(hWnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_clicado)
+            {
+                this.Left += (System.Windows.Forms.Control.MousePosition.X - this._lm.X);
+                this.Top += (System.Windows.Forms.Control.MousePosition.Y - this._lm.Y);
+                this._lm = System.Windows.Forms.Control.MousePosition;
+            }
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ReleaseMouseCapture();
+            _clicado = false;
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CaptureMouse();
+            _lm = System.Windows.Forms.Control.MousePosition;
+            _clicado = true;
+        }
+    }
+}
---
+++ Stopwatchscreen/TimeToLookAway.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using _20Min.Annotations;
+
+namespace _20Min
+{
+    public class TimeToLookAway : IDisposable, INotifyPropertyChanged
+    {
+        private readonly Timer _t;
+        private TimeSpan _restTime;
+        private string _value;
+
+        public TimeToLookAway()
+        {
+            this._restTime = TimeSpan.FromMinutes(20);
+            this.Value = $"Encore {this._restTime.TotalMinutes} min";
+            this._t = new Timer((state) =>
+            {
+                this._restTime = _restTime.Subtract(TimeSpan.FromMinutes(1));
+                this.Value = $"Encore {this._restTime.TotalMinutes} min";
+                if (_restTime.TotalMinutes <= 0)
+                {
+                    this.Value = "Regarde ailleurs !!!";
+                    this.RaiseLookAway();
+                    this._restTime = TimeSpan.FromMinutes(20);
+                }
+            }, null, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+        }
+
+        private void RaiseLookAway()
+        {
+            if (LookAway != null)
+            {
+                this.LookAway(this, new EventArgs());
+            }
+        }
+
+        public event EventHandler LookAway;
+
+        public string Value
+        {
+            get { return this._value; }
+            set
+            {
+                this._value = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public void Dispose()
+        {
+            this._t.Dispose();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
